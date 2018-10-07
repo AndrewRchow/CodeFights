511f31f1aacd41dd14bfb037d74bc9a24966db21
Andrew <chowlick@gmail.com>
2018-10-07 15:18:57 -0700

    test

--- Arcade/test.js
+++ Arcade/test.js
@@ -1,27 +1,66 @@
-/*var test = {b:1, a:2, 1:1};
-var test2 = {a:1, b:2};
+/*function count(arrayOfWords){
+    var result = {}
+    var current_count = -1;
+    var current_word = undefined;
+    for (var i = 0; i result[current_word]){
+            result[current_word] = current_count
+        }
+        console.log(result);
+    return result;
+}
 
+var a = ["book", "book", "hello", "job", "job", "job", "hello", "hello"] ;
+count(a);*/
 
+debugger;   
+var input = ["book", "book", "hello", "job", "job", "job", "hello", "hello", "hello", "hello","whack","boy","boy"];
 
-function check(a,b){
-  aKeys = Object.keys(a);
-  bKeys = Object.keys(b);
-  aValues = Object.values(a);
-  aValues = Object.values(b);
+function long(n) {
+  var output = {};
+  var current;
+  var count;
 
+  for(var i=0; i<n.length; i++){
+    if(i==0){
+      current = n[i];
+      count=1;
+    }
+    else{
+      if(n[i]==current){
+        count++;
 
-*/
+      }else{
+        if(current in output){
+          if(output[current]<count){
+            output[current]=count;
+          }
+        }else{
+          output[current]=count;
+        }
 
+        current = n[i];
+        count=1;
+      }
+    }
 
-var passed = 3;
+    if(i==n.length-1){
+       if(current in output){
+          if(output[current]<count){
+            output[current]=count;
+          }
+        }else{
+          output[current]=count;
+        }
+    }
 
-var addTo = function () {
-  var inner = 2;
-  return passed + inner;
-};
+    
+  }
 
-console.log(addTo())
+  return(output);
+}
 
+console.log(long(input));
 
-passed = 4;
-console.log(addTo())
+/*var a ={a:undefined};
+
+console.log(a.hasOwnProperty("a"));*/
